1ef1c93bd8df0fb8263cb89bba2d3bd4b02a701b
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-08-28 15:25:27 +0200

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Mail
+Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.*/
+
+
